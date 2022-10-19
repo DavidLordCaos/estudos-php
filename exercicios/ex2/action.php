8af0e6ed895f8b046ad4c4452380cd3057a32fc0
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-10-19 18:41:00 -0300

    arquivo alterado para ter somende a função que faz o calculo necessario da atividade

--- exercicios/ex2/action.php
+++ exercicios/ex2/action.php
@@ -1,22 +1,22 @@
 <?php
-$num = $_POST['num'];
 
-if ($num % 2 == 0 && $num % 5 == 0 && $num % 10 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 10, 5 e 2</div>";
-} else if ($num % 5 == 0 && $num % 10 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 10 e 5</div>";
-} else if ($num % 2 == 0 && $num % 10 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 10 e 2</div>";
-} else if ($num % 2 == 0 && $num % 5 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 5 e 2</div>";
-} else if ($num % 2 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 2</div>";
-} else if ($num % 5 == 0) {
-    echo "<div class='res'>O número $num é divisivel por 5</div>";
-} else {
-    echo "<div class='res'>O número $num não é divisivel por 2, 5 e 10</div>";
+function calculo($num)
+{
+    $num = $_POST['num'];
+
+    if ($num % 2 == 0 && $num % 5 == 0 && $num % 10 == 0) {
+        return "O número $num é divisivel por 10, 5 e 2";
+    } else if ($num % 5 == 0 && $num % 10 == 0) {
+        return "O número $num é divisivel por 10 e 5";
+    } else if ($num % 2 == 0 && $num % 10 == 0) {
+        return "O número $num é divisivel por 10 e 2";
+    } else if ($num % 2 == 0 && $num % 5 == 0) {
+        return "O número $num é divisivel por 5 e 2";
+    } else if ($num % 2 == 0) {
+        return "O número $num é divisivel por 2";
+    } else if ($num % 5 == 0) {
+        return "O número $num é divisivel por 5";
+    } else {
+        return "O número $num não é divisivel por 2, 5 e 10";
+    }
 }
-
-?>
-</body>
-</html>
