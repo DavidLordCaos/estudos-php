b389962b9db3df2dca175944c2970cb230856117
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-11-02 08:07:33 -0300

    separado exercicio em arquivos especificos de cada linguagem

--- exercicios/ex6/action.php
+++ exercicios/ex6/action.php
@@ -1,30 +1,36 @@
 <?php
-$mes = $_POST['mes'];
 
-if ($mes == 1) {
-    echo "Janeiro";
-} elseif ($mes == 2) {
-    echo "Fevereiro";
-} elseif ($mes == 3) {
-    echo "Março";
-} elseif ($mes == 4) {
-    echo "Abril";
-} elseif ($mes == 5) {
-    echo "Maio";
-} elseif ($mes == 6) {
-    echo "Junho";
-} elseif ($mes == 7) {
-    echo "Julho";
-} elseif ($mes == 8) {
-    echo "Agosto";
-} elseif ($mes == 9) {
-    echo "Setembro";
-} elseif ($mes == 10) {
-    echo "Outubro";
-} elseif ($mes == 11) {
-    echo "Novembro";
-} elseif ($mes == 12) {
-    echo "Dezembro";
-} else {
-    echo "Digite um número válido";
+function showMonth($mes)
+{
+    $mes = $_POST['mes'];
+
+    if ($mes == 1) {
+        $result = "Janeiro";
+    } elseif ($mes == 2) {
+        $result = "Fevereiro";
+    } elseif ($mes == 3) {
+        $result = "Março";
+    } elseif ($mes == 4) {
+        $result = "Abril";
+    } elseif ($mes == 5) {
+        $result = "Maio";
+    } elseif ($mes == 6) {
+        $result = "Junho";
+    } elseif ($mes == 7) {
+        $result = "Julho";
+    } elseif ($mes == 8) {
+        $result = "Agosto";
+    } elseif ($mes == 9) {
+        $result = "Setembro";
+    } elseif ($mes == 10) {
+        $result = "Outubro";
+    } elseif ($mes == 11) {
+        $result = "Novembro";
+    } elseif ($mes == 12) {
+        $result = "Dezembro";
+    } else {
+        $result = "Digite um número válido";
+    }
+    return $result;
 }
+
