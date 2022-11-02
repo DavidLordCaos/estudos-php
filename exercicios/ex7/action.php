d7c9062e8a1b8291f0cc1cd47eb2deb80520c118
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-11-02 11:23:55 -0300

    adicionado 7º exercicio e feita separação dos arquivos

--- exercicios/ex7/action.php
+++ exercicios/ex7/action.php
@@ -1,26 +1,13 @@
-<html>
-<body>
-<form action="ex7.php" method="post">
-    <label for="book">Informe o nome do livro: </label>
-    <input type="text" name="book" id="book"><br><br>
-    <input type="checkbox" id="prof" name="prof">
-    <label for="prof">Professor</label><br>
-    <input type="checkbox" id="aluno" name="aluno">
-    <label for="aluno">Aluno</label><br><br>
-    <input type="submit" value="Submit">
-</form>
-
 <?php
-$book = $_POST['book'];
-$prof = $_POST['prof'];
-$aluno = $_POST['aluno'];
 
-if ($prof) {
-    echo "Você tem 10 dias pra devolver o livro $book";
-} else {
-    echo "Você tem 3 dias pra devolver o livro $book";
+function returnBook($book, $choose) {
+    $book = $_POST['book'];
+    $choose = $_POST['choose'];
+
+    if ($choose == 'prof') {
+        $result = "Você tem 10 dias pra devolver o livro $book";
+    } else {
+        $result = "Você tem 3 dias pra devolver o livro $book";
+    }
+    return $result;
 }
-?>
-
-</body>
-</html>
