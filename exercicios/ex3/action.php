5a753e2f4ab0eb54500c675e8b84f72b7828fd36
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-11-14 05:38:19 -0300

    alterado metodo de verificação pois o tratamento é feito em outro arquivo sendo necessário retornar somente o nome na função

--- exercicios/ex3/action.php
+++ exercicios/ex3/action.php
@@ -1,14 +1,6 @@
 <?php
 
 function checkinResult($name) {
-    $sex = $_POST['genero'];
-    $age = $_POST['idade'];
-    $name = $_POST['nome'];
 
-    if ($sex === 'F' && $age < 25) {
-        $result = $name;
-    } else {
-        $result = $name;
-    }
-    return $result;
+    return $_POST['nome'];
 }
