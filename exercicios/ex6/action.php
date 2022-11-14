fb4a821c4e80e9370e6d7ede6e6e5a161e55d8c5
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-11-14 07:05:08 -0300

    criado array com os meses e passado metodo para retornar o mes correspondente ao número informado no formulario

--- exercicios/ex6/action.php
+++ exercicios/ex6/action.php
@@ -2,35 +2,7 @@
 
 function showMonth($mes)
 {
-    $mes = $_POST['mes'];
-
-    if ($mes == 1) {
-        $result = "Janeiro";
-    } elseif ($mes == 2) {
-        $result = "Fevereiro";
-    } elseif ($mes == 3) {
-        $result = "Março";
-    } elseif ($mes == 4) {
-        $result = "Abril";
-    } elseif ($mes == 5) {
-        $result = "Maio";
-    } elseif ($mes == 6) {
-        $result = "Junho";
-    } elseif ($mes == 7) {
-        $result = "Julho";
-    } elseif ($mes == 8) {
-        $result = "Agosto";
-    } elseif ($mes == 9) {
-        $result = "Setembro";
-    } elseif ($mes == 10) {
-        $result = "Outubro";
-    } elseif ($mes == 11) {
-        $result = "Novembro";
-    } elseif ($mes == 12) {
-        $result = "Dezembro";
-    } else {
-        $result = "Digite um número válido";
-    }
-    return $result;
+    $meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return $meses[$_POST['mes'] - 1];
 }
 
